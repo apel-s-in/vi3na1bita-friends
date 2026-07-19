144569b45f12568b7e73605097fff72433086d43
apel-s-in <133910431+apel-s-in@users.noreply.github.com>
2026-07-19 19:14:05 +0300

    Update friends-ui.js

--- friends-ui.js
+++ friends-ui.js
@@ -1,9 +1,9 @@
 // /Friends/friends-ui.js
 // UI поверх FriendsCore: список друзей + добавление (ссылка/код/QR/почта).
 
-import { getPlayableGames } from './games-registry.js';
-import { openTextChatModal } from './chat-text-ui.js';
-import { openVoiceCallUi } from './voice-call-ui.js';
+import { getPlayableGames } from './games-registry.js?v=8.8.2';
+import { openTextChatModal } from './chat-text-ui.js?v=8.8.2';
+import { openVoiceCallUi } from './voice-call-ui.js?v=8.8.2';
 
 const esc = v => String(v || '').replace(/[&<>"']/g, c => ({
   '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#039;'
